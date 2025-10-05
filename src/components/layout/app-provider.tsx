9ff5bc016e7d1427f6301a03f920e0a183af6cff
Ian Leakey <ian19leakey@gmail.com>
2025-10-05 16:26:47 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/layout/app-provider.tsx
+++ src/components/layout/app-provider.tsx
@@ -2,8 +2,19 @@
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { AppLayout } from "./app-layout";
+import { useEffect, useState } from "react";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return <AppLayout>{children}</AppLayout>;
+    }
+
     return (
         <ThemeProvider
           attribute="class"
