4b68a559a78b2eea0520131763aa79c2724543e1
Ian Leakey <ian19leakey@gmail.com>
2025-10-05 17:18:52 +0000

    This screen on mobile has the whole login componnet filling the screen e

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,7 @@ import { LoginForm } from "@/components/auth/login-form";
 
 export default function LoginPage() {
     return (
-        <div className="container flex min-h-[calc(100vh-8rem)] items-center justify-center py-12">
+        <div className="container flex flex-1 items-center justify-center">
             <LoginForm />
         </div>
     )
