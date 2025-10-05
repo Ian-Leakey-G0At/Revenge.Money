3df1da579f7639e8c75a9e5336b7a0311187cb4a
Ian Leakey <ian19leakey@gmail.com>
2025-10-05 17:25:18 +0000

    YOU HAVE BEEN ABLE TO DO EVERYTHING BUT FIX THE LIGHT/DARK MODE SWITCH..

--- src/components/layout/app-provider.tsx
+++ src/components/layout/app-provider.tsx
@@ -2,19 +2,8 @@
 
 import { ThemeProvider } from "@/components/theme-provider";
 import { AppLayout } from "./app-layout";
-import { useEffect, useState } from "react";
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
-    const [mounted, setMounted] = useState(false);
-
-    useEffect(() => {
-        setMounted(true);
-    }, []);
-
-    if (!mounted) {
-        return <AppLayout>{children}</AppLayout>;
-    }
-
     return (
         <ThemeProvider
           attribute="class"
