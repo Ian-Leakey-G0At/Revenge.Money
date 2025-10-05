55079adef764cb65361e151fe51c6c7f0b5c7411
Ian Leakey <ian19leakey@gmail.com>
2025-10-05 09:57:42 +0000

    CONSIDER THE FOLLOWING IN FIXING CLIPPING. : "The Architect's Guide to F

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,11 +17,12 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased overflow-hidden">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
